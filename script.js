93e1f8963dcf722e789d80ecedf75bd6d966faae
CirengMewing <mewingcireng@gmail.com>
2025-02-27 11:20:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,63 @@
 // sticky navbar
-document.addEventListener('scroll',() => {
-  const header = document.querySelector('header');
+document.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
 
   if (window.scrollY > 0) {
-    header.classList.add('scrolled');
+    header.classList.add("scrolled");
   } else {
-    header.classList.remove('scrolled')
+    header.classList.remove("scrolled");
   }
-})
+});
 
 // active links navbar
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
   let top = window.scrollY;
-  
-  sections.forEach(sec => {
+
+  sections.forEach((sec) => {
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach(link => {
-        link.classList.remove('active');
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
       });
-      document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
+      document
+        .querySelector('header nav a[href*="' + id + '"]')
+        .classList.add("active");
     }
   });
   closeNavbar(); // Menutup navbar saat scroll
 };
 
 // Toggle class active untuk hamburger menu
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 // ketika hamburger menu di klik
 menuIcon.onclick = () => {
-menuIcon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
 
 // Fungsi untuk menutup navbar
 function closeNavbar() {
-  navbar.classList.remove('active');
-  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove("active");
+  menuIcon.classList.remove("bx-x");
 }
 
 // Tutup Navbar saat klik di luar
-document.addEventListener('click', function (e) {
+document.addEventListener("click", function (e) {
   if (!menuIcon.contains(e.target) && !navbar.contains(e.target)) {
     closeNavbar();
   }
 });
 
 // Tutup Navbar saat klik menu item
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
     closeNavbar();
   });
 });
